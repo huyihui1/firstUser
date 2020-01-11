7a1b9b73331615e5c8ae4d180557b0b9d519cfa7
huyihui1 <xulinxuye@163.com>
2020-01-11 15:23:17 +0800

    添加less支持

---
+++ 07.react列表/react-tuts/config-overrides.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: huyihui
+ * @Date: 2020-01-11 13:27:50
+ * @LastEditTime : 2020-01-11 15:14:39
+ * @LastEditors  : Please set LastEditors
+ * @Description: 基于customize-cra 和react-app-rewired编辑配置文件
+ * @FilePath: \07.react列表\react-tuts\config-overrides.js
+ */
+//从customize-cra引入一些相关的方法
+const { override,addLessLoader} = require('customize-cra')
+module.exports = override(
+    addLessLoader({
+        javascriptEnabled: true,
+
+    })
+   
+)
