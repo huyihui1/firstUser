38a8c37f1a02d85e5d50232cec5ebfd3510df450
huyihui1 <xulinxuye@163.com>
2020-01-11 15:59:56 +0800

    配置less及antd

--- 07.react列表/react-tuts/config-overrides.js
+++ 07.react列表/react-tuts/config-overrides.js
@@ -1,17 +1,24 @@
 /*
  * @Author: huyihui
  * @Date: 2020-01-11 13:27:50
- * @LastEditTime : 2020-01-11 15:14:39
+ * @LastEditTime : 2020-01-11 15:57:38
  * @LastEditors  : Please set LastEditors
  * @Description: 基于customize-cra 和react-app-rewired编辑配置文件
  * @FilePath: \07.react列表\react-tuts\config-overrides.js
  */
 //从customize-cra引入一些相关的方法
-const { override,addLessLoader} = require('customize-cra')
+const { override,addLessLoader,fixBabelImports} = require('customize-cra')
+const theme = require('./lessVars')
 module.exports = override(
-    addLessLoader({
-        javascriptEnabled: true,
-
-    })
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: theme
+    }),
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true,
+  }),
+    
    
 )
--- 07.react列表/react-tuts/src/App.js
+++ 07.react列表/react-tuts/src/App.js
@@ -1,18 +1,20 @@
 /*
  * @Author: your name
  * @Date: 2020-01-11 13:23:53
- * @LastEditTime : 2020-01-11 15:18:26
+ * @LastEditTime : 2020-01-11 15:49:39
  * @LastEditors  : Please set LastEditors
  * @Description: In User Settings Edit'
  * @FilePath: \07.react列表\react-tuts\src\App.js
  */
 import React, { Component } from 'react'
+import {Button} from 'antd'
 import './index.less'
 export default class App extends Component {
     render() {
         return (
             <div>
               App
+              <Button type="primary">测试按钮</Button>
             </div>
         )
     }
